cdb7e5771b37f09c73c49c6567d284214d8f6eb5
tarekmansour <tarekmansour.net@gmail.com>
2023-09-12 21:12:16 +0200

    init db migration

---
+++ GraphQLSample.API/Migrations/20230912190928_InitialCreate.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraphQLSample.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sellers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Address = table.Column<string>(type: "varchar(250)", unicode: false, maxLength: 250, nullable: false),
+                    Country = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Contact = table.Column<string>(type: "varchar(150)", unicode: false, maxLength: 150, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sellers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Make = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Model = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Vin = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    TransimissionType = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    ModelYear = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    FirstRegistrationDate = table.Column<DateTime>(type: "datetime2", unicode: false, nullable: false),
+                    MileAge = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Color = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    SellerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vehicles_Sellers_SellerId",
+                        column: x => x.SellerId,
+                        principalTable: "Sellers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_SellerId",
+                table: "Vehicles",
+                column: "SellerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+
+            migrationBuilder.DropTable(
+                name: "Sellers");
+        }
+    }
+}
